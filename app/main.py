72af7a3bada3f04b950c30c87d32670f14b55aec
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-09-12 02:09:51 +0530

    added mongodb

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session
+from pymongo import MongoClient
+
+from app import config
+
 
 app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 #############WARNING: remove in production
+
+cluster = MongoClient(f'mongodb+srv://{config.DB_USERNAME}:{config.DB_PASSWORD}@cluster0.erdus.mongodb.net/database?retryWrites=true&w=majority')
+database = cluster['database']
+
 
 @app.route('/')
 def index_route():
