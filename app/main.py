45cc2394a55c93f065c11965507af8d6d07874d5
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-09-12 01:22:26 +0530

    layout set for index.html

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 #############WARNING: remove in production
+
 @app.route('/')
 def index_route():
     return render_template('index.html')
